41c48ff1a884d5f4631d34db1330e00feec381dc
marcodeiulio <mdeiulio@icloud.com>
2022-03-20 19:31:27 +0100

    - Printed db on page

--- app/Http/Controllers/TrainController.php
+++ app/Http/Controllers/TrainController.php
@@ -6,5 +6,9 @@ use App\Models\Train;
 
 class TrainController extends Controller
 {
-    //
+    public function show()
+    {
+        $trains = Train::all();
+        return view('home', compact('trains'));
+    }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,38 @@
 @extends('layouts.main')
 
 @section('content')
-home
+
+@foreach($trains as $train)
+<div class="container">
+	<div class="row>
+		<div class=" col-3">
+		<h1>Train n. {{$train->id}}</h1>
+		<ul class="list-group list-group-flush">
+			<li class="list-group-item">
+				<h3>Company: </h3>{{$train->company}}
+			</li>
+			<li class="list-group-item">
+				<h3>Departure Station: </h3>{{$train->departure_station}}
+			</li>
+			<li class="list-group-item">
+				<h3>Arrival Station: </h3>{{$train->arrival_station}}
+			</li>
+			<li class="list-group-item">
+				<h3>Departure Time: </h3>{{$train->departure_time}}
+			</li>
+			<li class="list-group-item">
+				<h3>Arrival Time: </h3>{{$train->arrival_time}}
+			</li>
+			<li class="list-group-item">
+				<h3>Train Number: </h3>{{$train->train_number}}
+			</li>
+			<li class="list-group-item">
+				<h3>Coaches: </h3>{{$train->coaches}}
+			</li>
+		</ul>
+	</div>
+</div>
+</div>
+@endforeach
+
 @endsection
