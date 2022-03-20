8345577f34f304ed03929def35e3427d06c6bb13
marcodeiulio <mdeiulio@icloud.com>
2022-03-20 19:11:24 +0100

    - Filled db using fill() method

---
+++ database/seeds/TrainsTableSeeder.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\Train;
+
+class TrainsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $trains = [
+            [
+                'company' => 'Trenitalia',
+                'departure_station' => 'Bologna Centrale',
+                'arrival_station' => 'Forlì',
+                'departure_time' => '11:50',
+                'arrival_time' => '12:52',
+                'train_number' => '453',
+                'coaches' => '7',
+                'is_late' => false,
+                'is_cancelled' => false
+            ],
+            [
+                'company' => 'Trenitalia',
+                'departure_station' => 'Forlì',
+                'arrival_station' => 'Bologna Centrale',
+                'departure_time' => '09:12',
+                'arrival_time' => '10:14',
+                'train_number' => '454',
+                'coaches' => '7',
+                'is_late' => true,
+                'is_cancelled' => false
+            ],
+        ];
+
+        foreach ($trains as $train) {
+            $new_train = new Train();
+            $new_train->fill($train);
+            $new_train->save();
+        }
+    }
+}
